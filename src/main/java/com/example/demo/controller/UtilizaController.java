67a8a7a981f8a00430130295f082b8169fc2d21e
alberto martin <albertombaldo@gmail.com>
2023-05-04 16:17:27 +0200

    creado controlador de Utiliza

---
+++ src/main/java/com/example/demo/controller/UtilizaController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UtilizaService;
+import com.example.demo.entity.Utiliza;
+
+@RestController
+@RequestMapping("/uso")
+public class UtilizaController {
+    @Autowired
+    UtilizaService us;
+
+    //localhost:8080/trazapp/uso?id=2
+	@GetMapping
+	public List<Utiliza> getUsos(@RequestParam Long id) {
+		return us.obtenerTodosUsos();	
+	}
+
+    @PostMapping
+	public ResponseEntity<?> altaReceta(@RequestBody Utiliza r) {
+		Utiliza uso = us.saveOrUpdateUso(r);
+		return new ResponseEntity("Uso dado de alta correctamente", HttpStatus.OK);
+	}
+}
