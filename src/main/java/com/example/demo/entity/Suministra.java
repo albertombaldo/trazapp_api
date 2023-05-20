67d738c1020801503cfa30ddd0f441c8591fea94
alberto <albertombaldo@gmail.com>
2023-05-20 23:23:56 +0200

    Incluida cascade en Suministra para actualizar stocks

--- src/main/java/com/example/demo/entity/Suministra.java
+++ src/main/java/com/example/demo/entity/Suministra.java
@@ -3,14 +3,7 @@ package com.example.demo.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,10 +21,10 @@ public class Suministra implements Serializable{
 	private Date fecha_recepcion;
 	@Temporal(TemporalType.DATE)
 	private Date fecha_caducidad;
-	@ManyToOne
-    @JoinColumn(name = "id_proveedor", referencedColumnName = "id_proveedor")	
+	@ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "id_proveedor", referencedColumnName = "id_proveedor")
 	private Proveedor proveedor;
-	@ManyToOne
+	@ManyToOne(cascade = {CascadeType.ALL})
     @JoinColumn(name = "id_producto", referencedColumnName = "id_producto")	
 	private Producto producto;
 	private String albaran;
